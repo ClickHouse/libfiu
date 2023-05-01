7835d077aecf71523ffc8967d2908becba982703
Han Fei <hanfei19910905@gmail.com>
2023-05-01 13:35:56 +0200

    add static keywords for some symbols

--- libfiu/hash.h
+++ libfiu/hash.h
@@ -11,7 +11,7 @@
 typedef struct hash hash_t;
 
 hash_t *hash_create(void (*destructor)(void *));
-void hash_free(hash_t *h);
+static void hash_free(hash_t *h);
 
 void *hash_get(hash_t *h, const char *key);
 bool hash_set(hash_t *h, const char *key, void *value);
